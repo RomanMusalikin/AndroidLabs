6691aa78d8fbbd0b8b19d09429721df9195aef7c
Roman <roman11122006@mail.ru>
2026-02-18 22:34:53 +0300

    Добавил возможность вводить текст с клавиатуры и пофиксил кнопки

--- LAB01/app/src/main/java/com/example/lab01/MainActivity.kt
+++ LAB01/app/src/main/java/com/example/lab01/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.lab01
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val phrases = listOf(
+            "Привет, Андроид!",
+            "Котлин — это круто!",
+            "Случайная фраза номер три",
+            "Улыбнитесь, вас снимает эмулятор! ХА-ХА-ХА-ХА-ХА (нет)",
+            "Программа работает исправно"
+        )
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
 
-        val textView: TextView = findViewById(R.id.textMain)
+        val textMain: EditText = findViewById(R.id.textMain)
         val btnInsertText: Button = findViewById(R.id.buttonSetText)
+        val btnClear: Button = findViewById(R.id.buttonClear)
 
         btnInsertText.setOnClickListener {
-            textView.text ="Это моя первая программа в Андроид!"
+            textMain.setText(phrases.random())
+        }
+
+        btnClear.setOnClickListener {
+            textMain.setText("")
         }
     }
 }
