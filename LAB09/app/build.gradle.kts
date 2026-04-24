1e28f18e7d43febec4e210e00c69fa649f6647d3
Roman <roman11122006@mail.ru>
2026-04-24 21:45:42 +0300

    Проблема с синхронизацией

--- LAB09/app/build.gradle.kts
+++ LAB09/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android) // Убрали "jetbrains", обычно в новых проектах так
+    kotlin("kapt")
 }
 
 android {
@@ -44,4 +46,29 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Версии библиотек
+    val work_version = "2.9.0"
+    val room_version = "2.6.1"
+    val retrofit_version = "2.11.0"
+    val lifecycle_version = "2.7.0"
+
+    // 1. WorkManager (Фоновые задачи)
+    implementation("androidx.work:work-runtime-ktx:$work_version")
+
+    // 2. Room (Локальная база данных)
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    "kapt"("androidx.room:room-compiler:$room_version")
+
+    // 3. Retrofit и Gson (Сетевые запросы и парсинг JSON)
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+
+    // 4. Lifecycle (ViewModel и LiveData)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // 5. Coroutines (Асинхронное программирование)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 }
